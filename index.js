c9c2e539451b1c518d267a5884d2d8107c85cdeb
Mihai Indreias <mihai.indreias@gmail.com>
2023-03-12 22:09:22 +0100

    redo commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio');
 const request = require('postman-request');
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('6096587498:AAHVjDrpTIUAQvrRhHsyeAsmflhVSk6VIco');
 
 let oldData = [];
 let newData=[];
@@ -40,14 +42,19 @@ const scrapePage= ()=>{
     setTimeout(()=>{
         console.log(`Currently there are ${newData.length} houses for rent in Delft.`);
         let newHouses=0;
+        let newHousesURLS=[];
         for(let i=0;i<newData.length;i++){
             if(!oldData.includes(newData[i])){
                 newHouses++;
+                newHousesURLS.push("https://www.funda.nl/"+newData[i]);
             }
         }
         console.log(`There are ${newHouses} new houses for rent in Delft.\n\n`);
-        oldData=newData;
+        oldData=[...new Set([...oldData,...newData])];
+        if(newHouses)
+            bot.telegram.sendMessage(-1001939899189,`Currently there are ${newData.length} houses for rent in Delft.\nThere are ${newHouses} new houses for rent in Delft.:${newHousesURLS.join('\n\n')}`);
     },3000);
+    
 };
 
-setInterval(scrapePage,30000);
+setInterval(scrapePage,60000);
